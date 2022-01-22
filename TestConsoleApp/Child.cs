6c9e85047baa37367c392ed4a25e80e1a33a3edf
Casey Niehaus <cjniehaus.cn@gmail.com>
2022-01-22 12:17:00 -0500

    initial commit

---
+++ TestConsoleApp/Child.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp {
+    internal class Child : Parent {
+
+        public Child() : this(0) {
+
+        }
+        public Child(int i) {
+            this.Id = i;
+        }
+    }
+}
